198b98c468526fae00a922d0a1690c2c6829699f
Ryoji-Nakao <meenmaikura@gmail.com>
2026-04-02 15:32:20 +0900

    Phase2: Spring Bootプロジェクト作成・初期設定

---
+++ backend/src/main/java/com/nakao/maneta/ManetaApplication.java
@@ -0,0 +1,13 @@
+package com.nakao.maneta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManetaApplication.class, args);
+	}
+
+}
